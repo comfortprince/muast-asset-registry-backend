b70a4a9179ef6d729e40e24ee621e72b48ca2c06
Comfort <comfort.prince.hluyo@gmail.com>
2026-05-06 10:46:01 +0200

    chore: Initialize Spring Boot backend project.

---
+++ src/main/java/ac/muast/it/asset_registry/AssetRegistryApplication.java
@@ -0,0 +1,13 @@
+package ac.muast.it.asset_registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssetRegistryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ac/muast/it/asset_registry/AssetRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package ac.muast.it.asset_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
